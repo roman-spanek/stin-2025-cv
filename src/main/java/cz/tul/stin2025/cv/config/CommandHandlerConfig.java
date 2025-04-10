abc8f10e06da88244182278204d9e94022b1ebf4
Roman_Spanek <roman.spanek@tul.cz>
2025-04-10 12:55:44 +0200

    DI

--- src/main/java/cz/tul/stin2025/cv/config/CommandHandlerConfig.java
+++ src/main/java/cz/tul/stin2025/cv/config/CommandHandlerConfig.java
@@ -1,6 +1,7 @@
 package cz.tul.stin2025.cv.config;
 
 import cz.tul.stin2025.cv.service.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,10 +12,10 @@ import java.util.List;
 public class CommandHandlerConfig {
 
     @Bean
-    public CommandHandler commandHandler() {
+    public CommandHandler commandHandler(@Value("${control.ligts}") String commnadlight) {
         CommandHandler commandHandler = new CommandHandler();
         HashMap<String, Command> commands = new HashMap<>();
-        commands.put("light", new LightsCommandImpl()); //?
+        commands.put(commnadlight, new LightsCommandImpl()); //?
         List<Command> windowCommands = List.of(new ACCommand(), new LightsCommandImpl());
         commands.put("window", new WindowCommandImpl(windowCommands)); //?
         commandHandler.setCommands(commands);
